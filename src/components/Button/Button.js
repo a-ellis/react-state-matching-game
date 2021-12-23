47a232387bc830ad3136c5f925e9955148652888
Alex Ellis <alexander.ellis@zendesk.com>
2021-12-23 16:05:24 -0600

    fixes for module 2

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Button.css";
 
 const Button = ({ startGame, playing }) => (
-  <button onClick={startGame}>{playing ? "Reset" : "Start"}</button>
+  <button onClick={startGame}>{playing ? "reset" : "start"}</button>
 );
 
 export default Button;
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -2,11 +2,11 @@ import React from "react";
 
 import "./Tile.css";
 
-const Tile = ({ selected, matched, color, svg }) => {
+const Tile = ({ selected, matched, color, svg: SVG }) => {
   const colorStyle = selected || matched ? { backgroundColor: color } : null;
   return (
     <div className="Tile" style={colorStyle}>
-      {selected || matched ? <svg /> : null}
+      {selected || matched ? <SVG /> : null}
     </div>
   );
 };
